3a604fbdae368cad6f0664c2a04dcbe639d1c433
LelouchL2 <cgcrew001@gmail.com>
2023-12-23 13:58:31 +0530

    feat: Add multiple banners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,72 @@
+"use client";
 import Banner from "@/components/Home/Banner";
-import Image from "next/image";
+import bannerImg1 from "../../public/images/Shadow-garden-intro-trans.png";
+import bannerImg2 from "../../public/images/1.png";
+import bannerImg3 from "../../public/images/upscaled.png";
+import { useState } from "react";
+
+const bannerDataList = [
+  {
+    title: "Shadow Garden Introduction ",
+    description: "Shadow garden introduction in the anime",
+    imageUrl: bannerImg1,
+    episodeNo: 1,
+  },
+  {
+    title: "Shadow VS Viscount Grease ",
+    description:
+      "Shadow is lost and doesn't know where the girls went while walking around the tunnels before he encounters Grease. He attacks Shadow, who easily blocks his attack.",
+    imageUrl: bannerImg1,
+    episodeNo: 2,
+  },
+  {
+    title: "School takeover ",
+    description: "Shadow garden introduction in the anime",
+    imageUrl: bannerImg2,
+    episodeNo: 3,
+  },
+  {
+    title: "I am Atomic ",
+    description: "Most epic moment",
+    imageUrl: bannerImg3,
+    episodeNo: 5,
+  },
+  // Add more objects with different values for the Banner component
+  // { title: "Another Title", description: "Another Description", ... },
+];
 
 export default function Home() {
+  const [selectedBannerIndex, setSelectedBannerIndex] = useState(0);
+
+  const handleDotClick = (index: number) => {
+    setSelectedBannerIndex(index);
+  };
+
   return (
-    <main className="flex flex-col items-center justify-between text-white">
-      <Banner />
+    <main className=" text-white">
+      <header
+        className="flex justify-between items-center 
+                  bg-gradient-to-t from-black/50 via-transparent to-transparent "
+      >
+        <ul className="flex flex-col pt-10 px-6">
+          {bannerDataList.map((_, index) => (
+            <li
+              key={index}
+              className={`w-3 h-3 rounded-full m-2 cursor-pointer hover:bg-primary 
+                          transition-all duration-400 ease-in-out 
+                          ${
+                            selectedBannerIndex === index
+                              ? "bg-primary"
+                              : "bg-muted"
+                          }`}
+              onClick={() => handleDotClick(index)}
+            ></li>
+          ))}
+        </ul>
+        <div className="transition-all duration-600 ease-in-out">
+          <Banner {...bannerDataList[selectedBannerIndex]} />
+        </div>
+      </header>
     </main>
   );
 }
--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -1,52 +1,52 @@
-import Image from "next/image";
-import bannerImg from "../../../public/images/Shadow-garden-intro-trans.png";
-import { Dot, MoveRight } from "lucide-react";
-const Banner = () => {
+import React from "react";
+import Image, { StaticImageData } from "next/image";
+import { MoveRight } from "lucide-react";
+import { FC } from "react";
+
+interface BannerProps {
+  title: string;
+  description: string;
+  episodeNo: number;
+  imageUrl: StaticImageData;
+}
+
+const Banner: FC<BannerProps> = ({
+  title,
+  description,
+  episodeNo,
+  imageUrl,
+}) => {
   return (
-    <main
+    <section
       className="
-      relative w-full h-[600px] 
-      bg-gradient-to-t from-black/50 via-transparent to-transparent
-      grid grid-row-3 grid-cols-[0.1fr,1fr,1fr] gap-1 pt-28"
+      relative w-full h-[600px]      
+      grid grid-row-[1fr,0.5fr,0.5fr] grid-cols-[0.8fr,1fr] gap-1 pt-28"
     >
-      <i className="col-start-1 col-end-2 row-start-1 row-end-4 flex flex-col gap-">
-        <Dot
-          color="white"
-          size={50}
-          strokeWidth={10}
-          absoluteStrokeWidth={true}
-        />
-        <Dot color="white" size={50} />
-        <Dot color="white" size={50} />
-        <Dot color="white" size={50} />
-      </i>
-      <h1 className=" text-7xl font-black text-primary leading-[90px] mx-2">
-        Shadow Garden Introduction
+      <h1 className="text-7xl font-black text-primary leading-[90px] mx-2 min-w-[800px] min-h-[160px]">
+        {title}
       </h1>
-      <p className="">Shadow garden introduction in the anime</p>
+      <p className="px-3">{description}</p>
       <button
         className="
         flex justify-around border items-center 
         border-white rounded-full text-base 
         ml-2 my-auto w-60 h-16 
-        col-start-2 col-end-3 row-start-3 row-end-4 "
+        col-start-1 col-end-2 row-start-3 row-end-4 "
       >
-        <p className="px-2">Watch Episode 01</p>
+        <p className="px-2">Watch Episode {episodeNo}</p>
         <MoveRight className="mr-2" />
       </button>
-      <div className="overflow-visible h-full col-start-3 col-end-4 row-start-1 row-end-4 -z-10">
+      <div className="overflow-visible h-full col-start-2 col-end-3 row-start-1 row-end-4 -z-10">
         <Image
-          width={1000}
+          width={950}
           height={560}
           sizes="(min-width: 1060px) 1000px, calc(94.59vw + 16px)"
           className="absolute bottom-0 right-0"
-          // className="w-[1000px] object-cover"
-          src={bannerImg}
+          src={imageUrl}
           alt="Banner image"
         />
-        {/* <img src={bannerImg} alt="banner image" /> */}
       </div>
-    </main>
+    </section>
   );
 };
 
